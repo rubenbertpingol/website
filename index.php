db80d74786060c7436aed378edca6d475f8e0434
Ruben Bert Pingol <prubenbert@gmail.com>
2013-03-30 18:36:15 +0100

    website current files

---
+++ index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html>
+    <head>
+				<meta name="google-site-verification" content="8BQixAS7Cj-wAA9C4zP61WLESVz2Gef4S_Ur9QVPGhc" />
+        <title>Ruben Bert Pingol's Portfolio</title>
+				<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+				<meta http-equiv="content-language" content="en-us" />
+				<meta name="Author" content="Ruben Bert T. Pingol" />
+        <meta name="Description" content="Portfolio of Ruben Bert T. Pingol" />
+        <meta name="Keywords" content="Portfolio, champion, 2013, tutor, job, graduate,web programmer, web developer, web designer, programmer, free lancer, lnu, leyte normal university, panelist, resource speaker, DOST scholar" />
+				<link rel="shortcut icon" type="image/png" href="img/lampara.png" />
+				<link rel="stylesheet" type="text/css" href="css/normalize.css" />
+				<link rel="stylesheet" type="text/css" href="css/media-all.css" />
+				<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+				<script src="js/functions.js"></script>
+    </head>
+
+    <body>
+      <nav>
+    		<div class="wrapper">
+  				<ul class="menu-horizontal menu top pull-left" >
+  					<li><a href="#">Home</a></li>
+  					<li><a href="#">Portfolio</a></li>
+  					<li><a href="#">Resume</a></li>
+  				</ul>
+  				<ul class="menu menu-horizontal top pull-right">
+  					<li><a href="#">Contact Me</a></li>
+  				</ul>
+  			</div>
+  		</nav>
+  		<div class="clearfix"></div>
+			
+			<header class="wrapper">
+					<h1 style="margin: 8px 0 0;padding: 5px 0;">Ruben Bert T. Pingol</h1>
+					<h2 style="margin: 0;padding: 0;font-weight: normal;font-family: tahoma; font-size: 14px;">Web developer, Tutor and Designer</h2>
+			</header>
+			
+			<section class="featured">
+  			<div class="wrapper">
+  				<div class="featured-box border-box">
+  					<div class="slider">
+  						<div class="slider-prev-button"><img src="img/slider-prev.png" /></div>
+  						<div class="slider-next-button"><img src="img/slider-next.png" /></div>
+  						<div class="slider-box"></div>
+  					</div>
+  					<aside>
+  						<h2>Contact Information</h2>
+  						<div class="trans-box">
+  							<address>
+  								<span><strong>Email address:</strong> <a href="https://plus.google.com/102271251501895438488?rel=author">prubenbert@gmail.com</a></span>
+  								<span><strong>Address:</strong> Brgy. 59-A 1st Street Sampaguita, Tacloban City</span>
+  								<span><strong>Contact number:</strong> +639128465460</span>
+  							</address>
+								<button style="color: #fff;" class="btn blue round-corner">Hire me now</button>
+  						</div>
+  					</aside>
+  				</div>
+  			</div>
+  		</section>			
+			
+  		<section class="content">
+  			<div class="wrapper">
+  				<div class="leftPane pull-left">
+  					<div class="pane-box border-box pad">
+  						<div class="content-box">
+								<article class="mission-vision">
+									<h1>About Ruben Bert</h1>
+									<hr/>
+									<p>
+										<strong>Ruben Bert T. Pingol</strong> born on May 16, 1993 is a web developer and a tutor. He was born and raised in Tacloban City, Leyte. Eldest child of Alberto Pingol and Ma. Nilda Pingol.
+									</p>
+									<p>
+										He finished his primary education at San Fernando Central School (SFCS). He had his secondary education at Cirilo Roy Montejo National High School (CRMNHS) and graduated as an honor student.
+									</p>
+									<p>
+										Ruben Bert's very first choice of course in college was Civil Engineering. He took the admission exam for Civil Engineering at Eastern Visayas University but upon waiting for the result of the examination, a letter coming from Department of Science and Technology (DOST) was received by his father stating that Ruben Bert was a qualifier of the DOST Scholarship Program. Ruben Bert passed also the admission exam for Civil Engineering. The DOST Scholarship Program was an opportunity for him and his family, so despite of passing the Civil Engineering, he took <strong>Bachelor of Science in Information Technology</strong> at <strong><a href="http://lnu.edu.ph/" target="_blank">Leyte Normal University</a></strong>. It was in his third year in college that he start his interest in <strong>Graphic designing</strong> and <strong>Web development.</strong>
+									</p>
+									<p>
+										He had his practicum at <strong><a href="http://www.allianceconsulting.biz/" target="_blank">Alliance Consulting Company</a></strong>. It was just a remote-based practicum since Ruben Bert and his group, work for the tasks given at their locality. He completed 240 working hours.
+									</p>
+									<p>
+										During his last year as a college student, he was told to team up with two of his classmates to form a programming team. The trio developed the <strong>LNU File Management System</strong> and became the <strong>Champion</strong> of the first<strong> WebCamp Hackathon 2013</strong> sponsored and organized by <a href="http://www.goabroad.com/" target="_blank">GoAbroad Philippines Inc.</a> (Tacloban Branch). He and one of his teammate were invited to be <strong>resource speakers</strong> in Northern Samar Colleges in Catarman, Northern Samar. He was one of a team started teaching second year and third year IT students of the university on web development. He was requested to be a <strong>panelist</strong> for the third year students' thesis mock defense and second year students' software project defense.
+									</p>
+									<p>
+										Ruben Bert and his group in thesis, won the <em>first place</em> in the <strong>Research Pageant</strong> held at Leyte Normal University. He finished his college being a DOST Scholar and was awarded <strong>Best Programmer.</strong>
+									</p>
+									<p>
+										Ruben Bert graduated and attained his bachelor's degree on March 2013.
+									</p>
+								</article>
+							</div>
+  					</div>
+  				</div>
+  				<div class="rightPane">
+  					<div class="aside-services">
+  						<h3 class="gold">Services Offered</h3>
+  					</div>
+  					<div class="pane-box border-box">
+  						<div class="content-box">
+  							<div class="asider-services">
+  								<ul class="menu-vertical menu services-menu">
+  									<li><a href="javascript: void(0);">Graphic Designing</a></li>
+  									<li><a href="javascript: void(0);">Web Designing</a></li>
+  									<li><a href="javascript: void(0);">Web-based Application Development</a></li>
+  									<li><a href="javascript: void(0);">Windows-based Application Development</a></li>
+  								</ul>
+  							</div>
+  						</div>
+  					</div>
+  					<div class="whitespace-divider"></div>
+						
+  					<div class="pane-box border-box round-corner">
+							<div class="content-box">
+								<h3 class="">Awards and Recognitions</h3>
+								<div class="aside-services">
+									<ul class="menu-vertical menu archives-menu">
+										<li><a href="javascript: void(0);"><span>&rsaquo;&rsaquo;</span>&nbsp;&nbsp;2013</a></li>
+										<li><a href="javascript: void(0);"><span>&rsaquo;&rsaquo;</span>&nbsp;&nbsp;2012</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						
+  					<div class="whitespace-divider"></div> 					
+  				</div>
+  				<div class="clearfix pull-left"></div>
+  			</div>
+  		</section>
+			
+			<!-- // End of content -->
+			
+  		<div class="clearfix"></div>
+  		<footer>
+  			<div class="wrapper">
+  				<div class="content-box">
+  					&copy; All rights reserved 2013. Developed and maintained by Ruben Bert Pingol. <em>(On development)</em>
+  				</div>
+  			</div>
+  		</footer>
+    </body>
+</html>
---
+++ js/functions.js
@@ -0,0 +1,21 @@
+(function($){
+	
+	$.fn.extend({
+			
+			myPageLoader:function(options){
+				
+				var defaults = {
+						
+						// contentBox: 
+						
+					};
+				
+				var obj = $.extend(defaults, options);
+				
+				
+				
+			}
+			
+		});
+	
+})(jQuery);
